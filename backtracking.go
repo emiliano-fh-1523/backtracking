e5d76a9fb2e34001c975a873dc429d5cc438d035
harelog3 <harel.olguin@iteso.mx>
2024-04-13 22:27:04 -0600

    initial setup

--- backtracking.go
+++ backtracking.go
@@ -2,6 +2,22 @@ package main
 
 import "fmt"
 
+func can_parse(input string) bool {
+	input = input
+	return false
+}
+
 func main() {
-    fmt.Println("Hello, world.")
+	inputs := []string{
+		"a+a*a+(a*a)",
+		"a++a",
+		"a",
+		"a+a",
+		"a(a)",
+		"(a+a)+a",
+	}
+
+	for _, input := range inputs {
+		fmt.Printf("Parsing input: %s -- %t\n", input, can_parse(input))
+	}
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module lang/backtracking
+
+go 1.22.1
